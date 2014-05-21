bc51e25789290a7cce73ad544535caac5ac5e730
Jeffrey Dill <jeffdill2@gmail.com>
2014-05-21 16:03:23 -0400

    jeffs test commit

--- test/spec/test.js
+++ test/spec/test.js
@@ -12,3 +12,17 @@
         });
     });
 })();
+
+//jeffs example
+(function () {
+    'use strict';
+
+    describe('Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+              var answer = 2;
+              expect(2).to.equal(answer);
+            });
+        });
+    });
+})();
