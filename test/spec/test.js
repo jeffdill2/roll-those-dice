44932cd85bb261991201c178613e35fa442bf7e8
Jeffrey Dill <jeffdill2@gmail.com>
2014-05-21 16:11:11 -0400

    Restored working files to original state

--- test/spec/test.js
+++ test/spec/test.js
@@ -12,24 +12,3 @@
         });
     });
 })();
-
-(function () {
-    'use strict';
-
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
-    });
-})();
-
-// example
-
-
-  // We shall contemplate truth by testing reality, via spec expectations.
-  it("should expect true", function() {
-    expect(true).toBeTruthy(); //This should be true
-  });
