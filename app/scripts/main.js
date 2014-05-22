86ee74e7875244c7a9d4b4cb7cc319674dfd37be
Jeffrey Dill <jeffdill2@gmail.com>
2014-05-22 16:44:44 -0400

    Added 'sides', 'countBy', and 'roll' tests -- created Dice() constructor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+function Dice(sides, countBy) {
+	this.sides = sides;
+	this.countBy = countBy;
+
+	this.roll = function() {
+		return Math.floor(Math.random() * this.sides + 1 ) * this.countBy;
+	};
+}
